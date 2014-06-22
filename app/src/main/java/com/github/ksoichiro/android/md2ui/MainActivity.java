47b8c3175faa0a83d0902cf3959633f1d18dd73d
ksoichiro <soichiro.kashima@gmail.com>
2014-06-22 20:26:01 +0900

    Extracted parsing logic to a class.

--- app/src/main/java/com/github/ksoichiro/android/md2ui/MainActivity.java
+++ app/src/main/java/com/github/ksoichiro/android/md2ui/MainActivity.java
@@ -26,7 +26,6 @@ import java.util.List;
 
 public class MainActivity extends FragmentActivity {
 
-    private Uri mUri;
     private List<Page> mPages;
     private ViewPager mPager;
     private boolean mFullscreen;
@@ -38,13 +37,13 @@ public class MainActivity extends FragmentActivity {
         mFullscreen = false;
 
         Intent intent = getIntent();
-        mUri = intent.getData();
-        if (mUri == null) {
+        Uri uri = intent.getData();
+        if (uri == null) {
             Toast.makeText(this, "Invalid URL", Toast.LENGTH_SHORT).show();
             finish();
             return;
         }
-        parse();
+        mPages = new Parser(uri).parse();
 
         mPager = (ViewPager) findViewById(R.id.pager);
         PageAdapter adapter = new PageAdapter(getSupportFragmentManager());
@@ -112,79 +111,5 @@ public class MainActivity extends FragmentActivity {
                 ab.show();
             }
         }
-    }
-
-    private void parse() {
-        mPages = new ArrayList<Page>();
-
-        List<String> lines = readFile(mUri);
-        Log.i("", "url: " + mUri);
-        Page page = new Page();
-        for (String line : lines) {
-            Log.i("", "line: " + line);
-            if (line.startsWith("---")) {
-                // New page
-                mPages.add(page);
-                page = new Page();
-                continue;
-            }
-            Content content = new Content();
-            content.content = line;
-            if (line.startsWith("# ")) {
-                // H1
-                content.contentType = ContentType.H1;
-                content.content = line.substring(2);
-            } else if (line.startsWith("## ")) {
-                // H2
-                content.contentType = ContentType.H2;
-                content.content = line.substring(3);
-            } else if (line.startsWith("* ") || line.startsWith("- ")) {
-                // ul/li
-                content.contentType = ContentType.LI;
-                content.content = line.substring(2);
-            } else if (line.startsWith("    * ") || line.startsWith("    - ")) {
-                // ul/li(2)
-                content.contentType = ContentType.LI2;
-                content.content = line.substring(6);
-            } else if (line.startsWith("        * ") || line.startsWith("        - ")) {
-                // ul/li(2)
-                content.contentType = ContentType.LI3;
-                content.content = line.substring(10);
-            } else if (!TextUtils.isEmpty(line)) {
-                // p
-                content.contentType = ContentType.P;
-            } else {
-                // ignore
-                continue;
-            }
-            page.contents.add(content);
-        }
-        mPages.add(page);
-    }
-
-    private List<String> readFile(Uri uri) {
-        final String charset = "UTF-8";
-        List<String> lines = new ArrayList<String>();
-        BufferedReader in = null;
-        try {
-            in = new BufferedReader(new InputStreamReader(
-                    new FileInputStream(new File(uri.getPath())),
-                    Charset.forName(charset)));
-            String line;
-            while ((line = in.readLine()) != null) {
-                lines.add(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return lines;
     }
 }
---
+++ app/src/main/java/com/github/ksoichiro/android/md2ui/Parser.java
@@ -0,0 +1,99 @@
+package com.github.ksoichiro.android.md2ui;
+
+import android.net.Uri;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parser {
+
+    private Uri mUri;
+
+    public Parser(final Uri uri) {
+        mUri = uri;
+    }
+
+    public List<Page> parse() {
+        List<Page> mPages = new ArrayList<Page>();
+
+        List<String> lines = readFile(mUri);
+        Log.i("", "url: " + mUri);
+        Page page = new Page();
+        for (String line : lines) {
+            Log.i("", "line: " + line);
+            if (line.startsWith("---")) {
+                // New page
+                mPages.add(page);
+                page = new Page();
+                continue;
+            }
+            Content content = new Content();
+            content.content = line;
+            if (line.startsWith("# ")) {
+                // H1
+                content.contentType = ContentType.H1;
+                content.content = line.substring(2);
+            } else if (line.startsWith("## ")) {
+                // H2
+                content.contentType = ContentType.H2;
+                content.content = line.substring(3);
+            } else if (line.startsWith("* ") || line.startsWith("- ")) {
+                // ul/li
+                content.contentType = ContentType.LI;
+                content.content = line.substring(2);
+            } else if (line.startsWith("    * ") || line.startsWith("    - ")) {
+                // ul/li(2)
+                content.contentType = ContentType.LI2;
+                content.content = line.substring(6);
+            } else if (line.startsWith("        * ") || line.startsWith("        - ")) {
+                // ul/li(2)
+                content.contentType = ContentType.LI3;
+                content.content = line.substring(10);
+            } else if (!TextUtils.isEmpty(line)) {
+                // p
+                content.contentType = ContentType.P;
+            } else {
+                // ignore
+                continue;
+            }
+            page.contents.add(content);
+        }
+        mPages.add(page);
+
+        return mPages;
+    }
+
+    private List<String> readFile(Uri uri) {
+        final String charset = "UTF-8";
+        List<String> lines = new ArrayList<String>();
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(
+                    new FileInputStream(new File(uri.getPath())),
+                    Charset.forName(charset)));
+            String line;
+            while ((line = in.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return lines;
+    }
+}
