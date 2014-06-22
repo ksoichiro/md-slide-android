f6dd18beb1d288e79445f49b4ea84330941b17b5
ksoichiro <soichiro.kashima@gmail.com>
2014-06-22 20:18:57 +0900

    Fixed problem when serializing.

--- app/src/main/java/com/github/ksoichiro/android/md2ui/Content.java
+++ app/src/main/java/com/github/ksoichiro/android/md2ui/Content.java
@@ -1,6 +1,8 @@
 package com.github.ksoichiro.android.md2ui;
 
-public class Content {
+import java.io.Serializable;
+
+public class Content implements Serializable {
 
     public ContentType contentType;
     public String content;
